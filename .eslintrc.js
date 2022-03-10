90ada3bc30e9f5b4ff72f09af3c0387a37a3be16
GeniusCorn <hi@nicecorn.com>
2022-03-10 19:39:06 +0800

    build: 更新配置文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,33 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:vue/essential', 'airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  ignorePatterns: ['vite.config.ts'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
     },
-    extends: ['plugin:vue/vue3-recommended', 'airbnb-base', 'prettier'],
-    parserOptions: {
-        ecmaVersion: 13,
-        parser: '@typescript-eslint/parser',
-        sourceType: 'module',
-    },
-    plugins: ['vue', '@typescript-eslint'],
-    ignorePatterns: ['vite.config.ts'],
-    rules: {},
+  },
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
 };
