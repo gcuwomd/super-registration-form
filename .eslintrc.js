7744a3896b474e67b51bf5ef5e0a379643e8a246
GeniusCorn <hi@nicecorn.com>
2022-03-12 11:05:38 +0800

    build: 更新依赖和配置文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,34 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['plugin:vue/essential', 'airbnb-base', 'prettier'],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module',
-  },
-  plugins: ['vue', '@typescript-eslint'],
-  ignorePatterns: ['vite.config.ts'],
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
+    env: {
+        browser: true,
+        es2021: true,
     },
-  },
-  rules: {
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-  },
+    extends: ['plugin:vue/vue3-recommended', 'airbnb-base', 'prettier'],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module',
+    },
+    plugins: ['vue', '@typescript-eslint'],
+    ignorePatterns: ['vite.config.ts'],
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            },
+        },
+    },
+    rules: {
+        'no-alert': 'off',
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never',
+            },
+        ],
+    },
 };
