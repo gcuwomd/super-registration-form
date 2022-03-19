0d34e63a02b5ac67e85deac771d2d3fd804b3c51
GeniusCorn <hi@nicecorn.com>
2022-03-19 12:58:02 +0800

    feat(datalist.vue): 列表分页

--- src/views/DataList.vue
+++ src/views/DataList.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, Ref } from 'vue';
 import FormService from '../service/FormService';
 import '@picocss/pico/css/pico.min.css';
 import IResponse from '../types/Response';
 import IForm from '../types/Form';
 
-const dataList = ref<IForm[]>();
+const dataList = ref<IForm[]>([]);
+
+const groupData = ref<any>([]);
+const pageSize = ref<number>(10);
+const pageNum: Ref<number> = ref(1);
+const dataShow = ref<IForm[]>([]);
+const currPage = ref<number>(0);
 
 onMounted(() => {
     FormService.getAll()
@@ -13,11 +19,37 @@ onMounted(() => {
             const res: IResponse = response.data;
 
             dataList.value = res.data as IForm[];
+
+            pageNum.value =
+                Math.ceil(dataList.value.length / pageSize.value) || 1;
+            for (let i = 0; i < pageNum.value; i += 1) {
+                groupData.value[i] = dataList.value.slice(
+                    pageSize.value * i,
+                    pageSize.value * (i + 1)
+                );
+            }
+            dataShow.value = groupData.value[currPage.value];
         })
         .catch((error) => {
             alert(error);
         });
 });
+
+const nextPage = () => {
+    if (currPage.value === pageNum.value - 1) {
+        return false;
+    }
+    dataShow.value = groupData.value[(currPage.value += 1)];
+    return true;
+};
+
+const prePage = () => {
+    if (currPage.value === 0) {
+        return false;
+    }
+    dataShow.value = groupData.value[(currPage.value -= 1)];
+    return true;
+};
 </script>
 
 <template>
@@ -36,7 +68,7 @@ onMounted(() => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="(data, index) in dataList" :key="index">
+                    <tr v-for="(data, index) in dataShow" :key="index">
                         <td>{{ index + 1 }}</td>
                         <td>
                             <router-link :to="`/details/${data.account}`">{{
@@ -45,11 +77,16 @@ onMounted(() => {
                         </td>
                         <td>{{ data.name }}</td>
                         <td>{{ data.academy }}</td>
-                        <td>{{ data.class }}</td>
+                        <td>{{ data.sClass }}</td>
                         <td>{{ data.time }}</td>
                     </tr>
                 </tbody>
             </table>
         </article>
+        <div class="grid">
+            <button @click="prePage">上一页</button>
+            <button @click="nextPage">下一页</button>
+        </div>
+        <div>当前第 {{ currPage + 1 }} 页，共 {{ pageNum }} 页</div>
     </div>
 </template>
