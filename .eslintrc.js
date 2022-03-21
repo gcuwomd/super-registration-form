6c0777d5c1716aa24609f7c0c0a084637b600ef7
GeniusCorn <hi@nicecorn.com>
2022-03-21 20:02:46 +0800

    refactor: 移除无用文件、依赖

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
+        'vue/setup-compiler-macros': true,
     },
     extends: ['plugin:vue/vue3-recommended', 'airbnb-base', 'prettier'],
     parserOptions: {
